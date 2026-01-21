fcf39f2a16b514d58279e6a71ffc471840762c41
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-21 22:20:48 +0600

    update dist file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/home';
-import About from './pages/about';
+import Home from './pages/Home';
+import About from './pages/About';
 import Courses from './pages/Courses';
 import Contact from './pages/Contact';
 import Navbar from './layout/Navbar';
