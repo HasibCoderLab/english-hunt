60b0e2f0b3ad4bcfbe409d8dc945007635f0d32f
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-21 13:10:45 +0600

    icons add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-//  import bannerImg from "../assets/bannerImg"
+import React from "react";
+import bannerImg from "../assets/Banner.jpg";
+import {
+  FaBook,
+  FaUserGraduate,
+  FaBookReader,
+  FaDeskpro,
+} from "react-icons/fa";
+
 const Home = () => {
   return (
-    <div>
-     <h1 className='text-2xl'>Hello</h1>
-     {/* <img src={bannerImg} alt="" /> */}
-    </div>
-  )
-}
+    <div className="w-full flex justify-center mt-10">
+      <div className="relative w-11/12">
 
-export default Home
+        {/* Banner Image */}
+        <img
+          src={bannerImg}
+          alt="banner"
+          className="w-full h-[320px] object-cover rounded-xl"
+        />
+
+        {/* Icons */}
+        <div className="absolute -top-6 left-10 bg-white p-3 rounded-full shadow-lg text-blue-600 text-2xl">
+          <FaBook />
+        </div>
+
+        <div className="absolute top-1/2 -left-6 bg-white p-3 rounded-full shadow-lg text-green-600 text-2xl">
+          <FaUserGraduate />
+        </div>
+
+        <div className="absolute top-1/2 -right-6 bg-white p-3 rounded-full shadow-lg text-purple-600 text-2xl">
+          <FaBookReader />
+        </div>
+
+        <div className="absolute -bottom-6 right-10 bg-white p-3 rounded-full shadow-lg text-orange-600 text-2xl">
+          <FaDeskpro />
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
