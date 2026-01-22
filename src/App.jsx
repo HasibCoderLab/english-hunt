a990c19fb3fb85c6f560f359ec24264c8b29b70c
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-22 10:59:44 +0600

    use extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
 import Courses from './pages/Courses.jsx';
 import Contact from './pages/Contact.jsx';
 import Navbar from './layout/Navbar.jsx';
